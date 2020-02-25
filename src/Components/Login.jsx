edbe959bb99065dd7a81ac4d3638a7f75f6e6b2f
Mostafa Rafea <mostafa.rafea19@gmail.com>
2020-02-25 02:21:28 +0200

    Include files

---
+++ src/Components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import background from './../img/bg elements.jpg';
+class Login extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <section className="container">
+
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Login;
---
+++ src/Components/User_Dashboard/Common/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './../../../img/logo 1.png';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
