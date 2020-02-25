7cbc27c970ddc9d5bfe376a54d805dd7dbbc73ef
Omar-Abdelrady <omarabdelrady123@gmail.com>
2020-02-25 21:42:40 +0200

    Omar's modifications base.css nav

--- src/Components/Charge.jsx
+++ src/Components/Charge.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './User_Dashboard/Common/Header';
 import Saidebar from './User_Dashboard/Common/Sidebar'
+import Navbar from "./User_Dashboard/Common/Navbar";
 
 class Charge extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <Header />
+                <Navbar />
                 <section className="Charge">
                     <div className="container">
                         <div className="row">
