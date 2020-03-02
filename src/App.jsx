447b8c687e99b642bc69da06352c4a08bfd45af7
Mostafa Rafea <mostafa.rafea19@gmail.com>
2020-03-03 00:30:53 +0200

    login, regestration & verification responsive

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,10 @@ import Shipments from "./Components/Shipments";
 import Verification from './Components/Verification';
 import Sent_Shipments from './Components/Sent_Shipments';
 import Landing_Page from './Components/Landing_Page';
-import ReactDOM from 'react-dom'
-import ReactWOW from 'react-wow'
 
 
 
 class App extends React.Component {
-
-
-
 
   state = {
     phone_number: '0501234567',
@@ -143,9 +138,6 @@ class App extends React.Component {
           </Route>
           <Route path="/sh">
             <Shipments />
-          </Route>
-          <Route path="/c">
-            <Statistics />
           </Route>
           <Route path="/register">
             <Register />
--- src/Components/User_Dashboard/Common/Footer.jsx
+++ src/Components/User_Dashboard/Common/Footer.jsx
@@ -7,14 +7,14 @@ class Footer extends React.Component {
             <React.Fragment>
                 <footer className="col-12 px-0">
                     <ul className="d-flex px-5 mx-lg-5 flex-md-row flex-column">
-                        <li className="px-2">شبكة ميسان للخدمات اللوجستية 2020</li>
-                        <li className="px-2">
+                        <li className="px-2 text-center">شبكة ميسان للخدمات اللوجستية 2020</li>
+                        <li className="px-2 text-center">
                             <a href="#">اتفاقية الاستخدام</a>
                         </li>
-                        <li className="px-2">
+                        <li className="px-2 text-center">
                             <a href="#">سياسة الخصوصية</a>
                         </li>
-                        <li className="px-2">
+                        <li className="px-2 text-center">
                             <a href="#">المساعدة</a>
                         </li>
                     </ul>
