ea12d40a03b2ef1d98b0865a054f2f31d2dc95d1
Mostafa Rafea <mostafa.rafea19@gmail.com>
2020-04-08 02:46:44 +0200

    Sidebar Modified

--- src/Components/User_Dashboard/Sent_Shipments.jsx
+++ src/Components/User_Dashboard/Sent_Shipments.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './layouts/Navbar';
 import Sidebar_ from './layouts/Sidebar';
 import Table from './Sent_Shipments_Table';
+import Footer from "./layouts/Footer";
 
 class Sent_Shipments extends React.Component {
 
@@ -16,14 +17,16 @@ class Sent_Shipments extends React.Component {
                             <h1 className="text-right mx-5 my-5 px-3">الشحنات المرسلة</h1>
                         </div>
                         <div className="row justify-content-around">
-                            <div className="">
+                            <div className="h-100">
                                 <Sidebar_/>
-
                             </div>
-                            <div className="col-9 px-0">
+                            <div className="col-9 px-0 h-100">
                                 <Table sent_shipments={this.props.sent_shipments}/>
                             </div>
                         </div>
+                    </div>
+                    <div className="my-5">
+                        <Footer/>
                     </div>
                 </section>
             </React.Fragment>
